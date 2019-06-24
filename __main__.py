702e6718e08ca1807f1da5b933daaf1787163d9e
Danny Foo <tiangsin.foo@gmail.com>
2019-06-25 01:23:51 +0800

    multi line RSS feeds

--- __main__.py
+++ __main__.py
@@ -1,10 +1,31 @@
+import feedparser
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "hello, world!"
+RSS_FEEDS = {
+    'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+    'cnn': 'http://rss.cnn.com/rss/edition.rss',
+    'fox': 'http://feeds.foxnews.com/foxnews/latest',
+    'iol': 'http://www.iol.co.za/cmlink/1.640'
+    }
+
+
+@app.route("/") 
+@app.route("/bbc")
+def bbc():
+    return get_news('bbc')
+
+@app.route("/cnn") 
+def cnn():
+    return get_news('cnn')
+
+def get_news(publication = "bbc"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return render_template(
+        "home.html",
+        articles = feed["entries"]
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
